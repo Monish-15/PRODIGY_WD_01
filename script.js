aafd73cd423d8b736e23d26ed4ffd7fa9f9dd229
Monish-15 <monishramgnanasekar@gmail.com>
2024-06-19 18:21:10 +0530

    Add

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// script.js
-
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 0) {
         navbar.style.backgroundColor = '#444';
     } else {
-        navbar.style.backgroundColor = '#333';
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
     }
 });
