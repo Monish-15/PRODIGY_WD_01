05fd52286ba67dbc28c075f5338822973335d33e
Monish-15 <monishramgnanasekar@gmail.com>
2024-06-19 18:16:39 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.style.backgroundColor = '#444';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
